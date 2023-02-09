501bc64c4bc679b1a0b23e388d2ad8beaf57ac90
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-02-09 09:25:14 -0500

    completed registration process

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require('mongoose')
+
 const bcryptjs = require('bcryptjs');
 const saltRounds = 10;
 
@@ -15,6 +17,19 @@ router.post('/signup', (req, res, next) => {
   console.log('The form data: ', req.body);
 
   const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.render('auth/signup.hbs', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+    return;
+  }
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res
+      .status(500)
+      .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+    return;
+  }
  
   bcryptjs
     .genSalt(saltRounds)
@@ -34,10 +49,20 @@ router.post('/signup', (req, res, next) => {
     })
     .then((userFromDB) => {
       console.log('Newly created user is: ', userFromDB);
+      res.redirect('/users/profile')
     })
-    .catch(error => next(error));
+    .catch(error => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render('auth/signup', { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        res.status(500).render('auth/signup', {
+           errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+        });
+      } else {
+        next(error);
+      }
+    })
 
-  res.redirect('/users/profile')
 
 })
 
